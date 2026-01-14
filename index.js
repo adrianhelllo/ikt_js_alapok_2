d9c6e4d6d65fbae390dd485478f16915f1a133de
adrianhelllo <hetessyadrian@gmail.com>
2026-01-14 19:35:24 +0100

    scanning document contents

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function swap_img() {
     img_num = (Number(cur_src[cur_src.length - 5]) + 1) % 2
     img.src = `src/swap_img${img_num}.png`
 }
+
+function scan_contents() {
+    message = document.body.innerHTML
+    alert(message)
+}
