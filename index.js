29aba5ab46c145d5e61a1a1a9250d34cec9c1ff6
adrianhelllo <hetessyadrian@gmail.com>
2026-01-14 19:54:56 +0100

    Wow

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ function scan_contents() {
     message = document.body.innerHTML
     alert(message)
 }
+
+function change_text() {
+    text = document.getElementById("change_p")
+    text.innerHTML = text.innerHTML.split("").reverse().join("")
+}
+
+function change_vis() {
+    text = document.getElementById("change_v")
+    if (text.style.display == "inline") {
+        text.style.display = "none"
+    }
+    else {
+        text.style.display = "inline"
+    }
+}
+
+function increment_c() {
+    counter = document.getElementById("counter")
+    counter.innerHTML = Number(counter.innerHTML) + 1
+    counter.classList.add("spin")
+    
+    counter.addEventListener("animationend", () => {
+        counter.classList.remove("spin")
+    })
+}
